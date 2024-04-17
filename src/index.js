9df9cc97df52f86de0f863375a44679381e1e9d8
Julian Cox <julian.cox@webspeed.co.nz>
2024-04-17 16:15:01 +1200

    changed compass needle to web component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-export { compassNeedle } from './lib/compass-needle.js';
+import { compassNeedle } from './lib/compass-needle.js';
+
+
+export class GeocamViewerCompassNeedle extends HTMLElement {
+  constructor() {
+    super();
+    this.plugin = null;
+    // this.yaw = this.getAttribute('yaw') || 0;
+    console.log("comapss-needle init");
+  }
+
+  connectedCallback() {
+    console.log("comapss-needle connected");
+    const node = this;
+    this.plugin = new compassNeedle();
+    const parent = this.parentNode;
+    if (parent.viewer && parent.viewer.plugin) {
+      // Call a method on the parent
+      parent.viewer.plugin(this.plugin);
+    } else {
+      console.error(
+        "GeocamViewerCompassNeedle must be a child of GeocamViewer"
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    this.plugin = null;
+    console.log("comapss-needle disconnected");
+    // Clean up the viewer
+  }
+}
+
+window.customElements.define(
+  "geocam-viewer-compass-needle",
+  GeocamViewerCompassNeedle
+);
