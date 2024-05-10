dd3173b572fc8637fff7bb8a0aa91b263ab0fb50
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-10 16:09:29 +1200

    Update to version v2.0.3 in plugins/compass-needle

--- dist/compass-needle.js
+++ dist/compass-needle.js
@@ -1,10 +1,10 @@
-const l = (o, e = {}, t = "") => {
-  const n = document.createElement(o);
+const l = (t, e = {}, o = "") => {
+  const n = document.createElement(t);
   for (let s in e)
     n.setAttribute(s, e[s]);
-  return n.innerHTML = t, n;
-}, a = (o, e) => (document.getElementById(o) || document.getElementsByTagName("head")[0].prepend(l("STYLE", { type: "text/css" }, e)), !0), d = function(o = {}) {
-  let e, t, n;
+  return n.innerHTML = o, n;
+}, a = (t, e) => (document.getElementById(t) || document.getElementsByTagName("head")[0].prepend(l("STYLE", { type: "text/css" }, e)), !0), r = function(t = {}) {
+  let e, o, n;
   a("geocam-compass-needle", `
   .geocam-compass-needle {
       background-image:
@@ -14,33 +14,33 @@ const l = (o, e = {}, t = "") => {
       width: 64px;
       height: 64px;
     }
-  `), this.init = function(i) {
-    e = i, n = l("DIV", { class: "geocam-compass-needle" }), e.addControl(n, "right-top"), t = e.facing((c) => {
-      c !== null && (n.style.transform = `rotate(${360 - c}deg)`);
+  `), this.init = function(c) {
+    e = c, n = l("DIV", { class: "geocam-compass-needle" }), e.addControl(n, "right-top"), o = e.facing((i) => {
+      i !== null && (n.style.transform = `rotate(${360 - i}deg)`);
     });
   }, this.destroy = function() {
-    t(), e.wrapper.removeChild(n);
+    o(), e.wrapper.removeChild(n);
   };
 };
-class r extends HTMLElement {
+class d extends HTMLElement {
   constructor() {
     super(), this.plugin = null, console.log("comapass-needle init");
   }
   connectedCallback() {
-    console.log("comapass-needle connected");
+    console.log("comapass-needle connected"), this.plugin = new r();
     const e = this.parentNode;
-    e.viewer && e.viewer.plugin ? (this.plugin = new d(), e.viewer.plugin(this.plugin)) : console.error(
+    this.viewer = e.viewer, this.viewer && this.viewer.plugin ? this.viewer.plugin(this.plugin) : console.error(
       "GeocamViewerCompassNeedle must be a child of GeocamViewer"
     );
   }
   disconnectedCallback() {
-    this.plugin = null, console.log("comapass-needle disconnected");
+    this.plugin = null, this.viewer = null, console.log("comapass-needle disconnected");
   }
 }
 window.customElements.define(
   "geocam-viewer-compass-needle",
-  r
+  d
 );
 export {
-  r as GeocamViewerCompassNeedle
+  d as GeocamViewerCompassNeedle
 };
