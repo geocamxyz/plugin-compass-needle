262d742ceb7b9a9dbc2d9d605fe1de95ac6da2e3
Luke Reid <luke@reid.org.nz>
2025-11-04 15:26:12 +1300

    Guard plugin registration until geocam-viewer ready

--- src/index.js
+++ src/index.js
@@ -4,27 +4,40 @@ export class GeocamViewerCompassNeedle extends HTMLElement {
   constructor() {
     super();
     this.plugin = null;
+    this.viewer = null;
     // this.yaw = this.getAttribute('yaw') || 0;
     console.log("comapass-needle init");
   }
 
   connectedCallback() {
     console.log("comapass-needle connected");
-    const node = this;
-    this.plugin = new compassNeedle();
-    const parent = this.parentNode;
-    this.viewer = parent.viewer;
-    if (this.viewer && this.viewer.plugin) {
-      // Call a method on the parent
-      this.viewer.plugin(this.plugin);
-    } else {
+    const host = this.closest("geocam-viewer");
+    if (!host) {
       console.error(
         "GeocamViewerCompassNeedle must be a child of GeocamViewer"
       );
+      return;
     }
+
+    const attach = () => {
+      const viewer = host.viewer;
+      if (viewer && typeof viewer.plugin === "function") {
+        if (this.plugin) return;
+        this.viewer = viewer;
+        this.plugin = new compassNeedle();
+        this.viewer.plugin(this.plugin);
+      } else {
+        setTimeout(attach, 50);
+      }
+    };
+
+    attach();
   }
 
   disconnectedCallback() {
+    if (this.plugin && typeof this.plugin.destroy === "function") {
+      this.plugin.destroy();
+    }
     this.plugin = null;
     this.viewer = null;
     console.log("comapass-needle disconnected");
